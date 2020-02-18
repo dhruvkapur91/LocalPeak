e8944bc7e295b70a96c6d7862ca3884ad29dbf4e
dhruvk <dhruvkapur91@gmail.com>
2020-02-18 16:59:59 +0530

    Test. Can also find local peak of all duplicates

--- test_local_peak_finder.py
+++ test_local_peak_finder.py
@@ -9,3 +9,7 @@ class TestLocalPeakFinder(unittest.TestCase):
     def test_local_peak_of_single_element_is_itself(self):
         self.assertEqual(0, local_peak([0]))
         self.assertEqual(1, local_peak([1]))
+
+    def test_local_peak_of_two_same_elements_is_any_of_them(self):
+        self.assertEqual(0, local_peak([0, 0]))
+        self.assertEqual(1, local_peak([1, 1]))
