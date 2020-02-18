2dbe2203a2f4e48c1bbe9e17207414e2f0a71644
dhruvk <dhruvkapur91@gmail.com>
2020-02-18 17:07:34 +0530

    Add. Can find local peak in an ascending order of 3 elements

--- test_local_peak_finder.py
+++ test_local_peak_finder.py
@@ -2,8 +2,13 @@ import unittest
 
 
 def local_peak(numbers):
-    if len(numbers) > 1:
+    if len(numbers) == 2:
         return max(numbers[0], numbers[1])
+    if len(numbers) == 3:
+        if numbers[0] >= numbers[1]:
+            return numbers[0]
+        else:
+            return local_peak(numbers[1:])
     return numbers[0]
 
 
@@ -19,3 +24,6 @@ class TestLocalPeakFinder(unittest.TestCase):
     def test_local_peak_of_two_elements_is_the_greater_one_of_the_two(self):
         self.assertEqual(1, local_peak([1, 0]))
         self.assertEqual(1, local_peak([0, 1]))
+
+    def test_local_peak_of_three_elements_is_the_greatest_one_of_the_three(self):
+        self.assertEqual(2, local_peak([0, 1, 2]))
