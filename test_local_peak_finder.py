f5a4af718974d26f9be0781e790c153eba54aedf
dhruvk <dhruvkapur91@gmail.com>
2020-02-18 16:58:58 +0530

    Add. Find local peak of any single element collection

--- test_local_peak_finder.py
+++ test_local_peak_finder.py
@@ -2,9 +2,10 @@ import unittest
 
 
 def local_peak(numbers):
-    return 0
+    return numbers[0]
 
 
 class TestLocalPeakFinder(unittest.TestCase):
     def test_local_peak_of_single_element_is_itself(self):
         self.assertEqual(0, local_peak([0]))
+        self.assertEqual(1, local_peak([1]))
