0e0ee896e617382a0850398d26547e01b9fed97f
dhruvk <dhruvkapur91@gmail.com>
2020-02-18 17:02:24 +0530

    Add. Can find local peak in any 2 element collection

--- test_local_peak_finder.py
+++ test_local_peak_finder.py
@@ -2,6 +2,8 @@ import unittest
 
 
 def local_peak(numbers):
+    if len(numbers) > 1:
+        return max(numbers[0], numbers[1])
     return numbers[0]
 
 
@@ -13,3 +15,7 @@ class TestLocalPeakFinder(unittest.TestCase):
     def test_local_peak_of_two_same_elements_is_any_of_them(self):
         self.assertEqual(0, local_peak([0, 0]))
         self.assertEqual(1, local_peak([1, 1]))
+
+    def test_local_peak_of_two_elements_is_the_greater_one_of_the_two(self):
+        self.assertEqual(1, local_peak([1, 0]))
+        self.assertEqual(1, local_peak([0, 1]))
